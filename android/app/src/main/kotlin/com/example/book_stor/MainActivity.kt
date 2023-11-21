69cc12dc8fe99e8d21703f350459f8e802619ed3
kerolus77 <125474671+kerolus77@users.noreply.github.com>
2023-11-21 16:58:57 +0200

    book_store

---
+++ android/app/src/main/kotlin/com/example/book_stor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.book_stor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:book_stor/Features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BookStore());
+}
+
+
+class BookStore extends StatelessWidget {
+  const BookStore({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: SplashView(),
+    );
+  }
+}
